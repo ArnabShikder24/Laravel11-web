154d220e6d87a6abfb8b834e38be68230f5e84f7
ArnabShikder24 <arnabshikder2001@gmail.com>
2024-11-23 20:39:58 +0600

    create page

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -6,19 +6,62 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
     <title>Lravel 11!</title>
   </head>
   <body>
-    <div class="bg-black py-3">
-      <h1 class="text-whtie">Simple Laravel 11</h1>
+    <div class="bg-black p-3">
+      <h1 class="text-white h3 text-center">Simple Laravel 11 CRUD</h1>
+    </div>
+
+    <div class="container">
+        <div class="row justify-content-center mt-4">
+            <div class="col-md-10 d-flex justify-content-end">
+                <a class="btn btn-dark">Back</a>
+            </div>
+        </div>
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-10">
+                <div class="card borde-0 shadow-lg my-4">
+                    <div class="card-header bg-dark">
+                        <h3 class="text-white">Create Product</h3>
+                    </div>
+                    <form>
+                        <div class="card-body">
+                            <div class="mb-3">
+                                <label for="" class="form-label h5">Name</label>
+                                <input type="text" class="form-control-lg form-control" placeholder="Name" name="name">
+                            </div>
+                            <div class="mb-3">
+                                <label for="" class="form-label h5">Sku</label>
+                                <input type="text" class="form-control form-control-lg" placeholder="Sku" name="sku">
+                            </div>
+                            <div class="mb-3">
+                                <label for="" class="form-label h5">Price</label>
+                                <input type="text" class="form-control form-control-lg" placeholder="Price" name="price">
+                            </div>
+                            <div class="mb-3">
+                                <label for="" class="form-label h5">Description</label>
+                                <textarea placeholder="Description" class="form-control" name="description" cols="30" rows="5">description</textarea>
+                            </div>
+                            <div class="mb-3">
+                                <label for="" class="form-label h5">Image</label>
+                                <input type="file" class="form-control form-control-lg" placeholder="Price" name="image">
+                            </div>
+                            <div class="d-grid">
+                                <button class="btn btn-lg btn-primary">Submit</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
   </body>
 </html>
